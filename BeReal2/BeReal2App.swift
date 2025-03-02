74cad059c54a7fd141d885a6af31c5f80e4a87c7
Janniel Tan <tanjanniel@gmail.com>
2025-03-02 12:58:15 -0800

    Initial Commit

---
+++ BeReal2/BeReal2App.swift
@@ -0,0 +1,17 @@
+//
+//  BeReal2App.swift
+//  BeReal2
+//
+//  Created by Janniel Tan on 3/2/25.
+//
+
+import SwiftUI
+
+@main
+struct BeReal2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
